35593ec2251a2e0d50f1a7f5c31adbf5acdcc22b
Vít Pekárek <pekarvit@fit.cvut.cz>
2017-07-27 11:27:02 +0200

    Změna řazení fotek při editaci

--- app/PhotoModule/presenters/AlbumPresenter.php
+++ app/PhotoModule/presenters/AlbumPresenter.php
@@ -16,49 +16,49 @@ use lsolesen\pel\PelJpeg;
 use Tracy\Debugger;
 use WebChemistry\Forms\Controls\Multiplier;
 
-class AlbumPresenter extends BasePresenter{
+class AlbumPresenter extends BasePresenter {
 
-	 /** @var GalleryService @inject*/
+	/** @var GalleryService @inject */
 	public $gallery;
 
-	/** @var MemberService @inject*/
+	/** @var MemberService @inject */
 	public $members;
 
 	/** @var \Echo511\Plupload\Control\IPluploadControlFactory @inject */
 	public $controlFactory;
 
-	public function getAlbumById($slug){
+	public function getAlbumById($slug) {
 		$id = $this->getIdFromSlug($slug);
 
 		$album = $this->gallery->getAlbumById($id);
 
-		if ((!$album)or($slug != $album->id.'-'.$album->slug)){
-			$this->flashMessage('Zadané album neexistuje','error');
+		if ((!$album) or ($slug != $album->id . '-' . $album->slug)) {
+			$this->flashMessage('Zadané album neexistuje', 'error');
 			$this->redirect('default');
 		}
 
 		return $album;
 	}
 
-	public function renderDefault(){
+	public function renderDefault() {
 		$albums = $this->gallery->getAlbums()->order('date DESC');
 		$pocet = $this->gallery->getAlbumsPhotosCount();
 
-		if (!$this->getUser()->isLoggedIn()){
-			$albums->where('visible',TRUE);
-			$pocet->where('album.visible',TRUE)->where(':photo.visible',TRUE);
-		}else $this->template->member = $this->members->getMembersArray(FALSE);
+		if (!$this->getUser()->isLoggedIn()) {
+			$albums->where('visible', TRUE);
+			$pocet->where('album.visible', TRUE)->where(':photo.visible', TRUE);
+		} else $this->template->member = $this->members->getMembersArray(FALSE);
 
 		$this->template->albums = $albums;
-		$this->template->pocet = $pocet->fetchPairs('id','pocet');
+		$this->template->pocet = $pocet->fetchPairs('id', 'pocet');
 	}
 
-	public function renderUsers(){
+	public function renderUsers() {
 		$this->checkUserLoggin();
 
 		$albums = $this->gallery->getAlbums()->order('member_id, date DESC');
 
-		$membersAlbums = $albums->fetchPairs('member_id','id');
+		$membersAlbums = $albums->fetchPairs('member_id', 'id');
 
 		$member = $this->members->getMembersArray(FALSE);
 		asort($member);
@@ -66,7 +66,7 @@ class AlbumPresenter extends BasePresenter{
 		$membersAlbums = array_intersect_key($member, $membersAlbums);
 
 		$pocet = $this->gallery->getAlbumsPhotosCount();
-		$pocet = $pocet->fetchPairs('id','pocet');
+		$pocet = $pocet->fetchPairs('id', 'pocet');
 
 		$this->template->albums = $albums;
 		$this->template->pocet = $pocet;
@@ -74,19 +74,19 @@ class AlbumPresenter extends BasePresenter{
 		$this->template->membersAlbums = $membersAlbums;
 	}
 
-	public function renderView($slug,$pubkey = null){
+	public function renderView($slug, $pubkey = null) {
 		$album = $this->getAlbumById($slug);
 		$this->template->album = $album;
 
 		$pubkeyCheck = $pubkey === $album->pubkey;
 
-		if ((!$album->visible)and(!(($this->getUser()->isLoggedIn())or($pubkeyCheck)))) $this->checkUserLoggin();
+		if ((!$album->visible) and (!(($this->getUser()->isLoggedIn()) or ($pubkeyCheck)))) $this->checkUserLoggin();
 
 		$photos = $this->gallery->getPhotosByAlbumId($album->id)->order('order, date_add');
 
 		$member = $this->members->getMemberById($album->member_id);
 
-		if (!(($this->getUser()->isLoggedIn())or($pubkeyCheck))) $photos->where('visible',TRUE);
+		if (!(($this->getUser()->isLoggedIn()) or ($pubkeyCheck))) $photos->where('visible', TRUE);
 
 		$this->template->photos = $photos;
 		$this->template->slug = $slug;
@@ -94,23 +94,26 @@ class AlbumPresenter extends BasePresenter{
 
 	}
 
-	public function renderEdit($slug, $order = 'order'){
+	public function renderEdit($slug, $order = 'order') {
 		$this->checkUserLoggin();
 
 		$album = $this->getAlbumById($slug);
 		$this->template->album = $album;
 
-		if (!(($album->member_id == $this->getUser()->getId())or($this->getUser()->isInRole($this->name)))) {
-			$this->flashMessage('Nemáte právo toho album upravovat','error');
-			$this->redirect('view',$slug);
+		if (!(($album->member_id == $this->getUser()->getId()) or ($this->getUser()->isInRole($this->name)))) {
+			$this->flashMessage('Nemáte právo toho album upravovat', 'error');
+			$this->redirect('view', $slug);
 		}
+
+		$this->template->order = $order;
+
+		$order = ($order == 'order') ? $order . ', date_add' : $order;
 
 		$photos = $this->gallery->getPhotosByAlbumId($album->id)
 			->order($order);
 
 		$this->template->photos = $photos;
 		$this->template->slug = $slug;
-		$this->template->order = $order;
 
 		$form = $this['superForm'];
 		if (!$form->isSubmitted()) {
@@ -125,7 +128,7 @@ class AlbumPresenter extends BasePresenter{
 		}
 	}
 
-	public function renderAdd($slug){
+	public function renderAdd($slug) {
 		$this->checkUserLoggin();
 
 		$album = $this->getAlbumById($slug);
@@ -133,57 +136,57 @@ class AlbumPresenter extends BasePresenter{
 		$this->template->slug = $slug;
 	}
 
-	public function actionSetAlbumVisibility($slug, $visible = FALSE){
+	public function actionSetAlbumVisibility($slug, $visible = FALSE) {
 		$this->checkUserLoggin();
 
 		$id = $this->getIdFromSlug($slug);
 
 		if (!$this->getUser()->isInRole($this->name)) {
-			$this->flashMessage('Nemáte právo měnit viditelnost alba','error');
-			$this->redirect('view',$slug);
+			$this->flashMessage('Nemáte právo měnit viditelnost alba', 'error');
+			$this->redirect('view', $slug);
 		}
 
 		$this->gallery->getAlbumById($id)->update(['visible' => $visible]);
 
 		$text = $visible ? null : 'ne';
-		$this->flashMessage('Album bylo označeno jako '.$text.'viditelné pro veřenost');
+		$this->flashMessage('Album bylo označeno jako ' . $text . 'viditelné pro veřenost');
 
-		$this->redirect('Album:view',$slug);
+		$this->redirect('Album:view', $slug);
 	}
 
-	public function actionDeleteAlbum($id){
-	   $this->checkUserLoggin();
+	public function actionDeleteAlbum($id) {
+		$this->checkUserLoggin();
 
-	   $album = $this->gallery->getAlbumById($id);
+		$album = $this->gallery->getAlbumById($id);
 
-	   if (!(($album->member_id==$this->getUser()->getId())or($this->getUser()->isInRole($this->name)))) {
-		$this->flashMessage('Nemáte právo toto album smazat','error');
-		$this->redirect('view',$id.'-'.$album->slug);
-	   }
+		if (!(($album->member_id == $this->getUser()->getId()) or ($this->getUser()->isInRole($this->name)))) {
+			$this->flashMessage('Nemáte právo toto album smazat', 'error');
+			$this->redirect('view', $id . '-' . $album->slug);
+		}
 
-	   $this->gallery->getPhotosByAlbumId($album->id)->delete();
-	   $album->delete();
+		$this->gallery->getPhotosByAlbumId($album->id)->delete();
+		$album->delete();
 
-	   $this->flashMessage('Album bylo smazáno');
-	   $this->redirect('Myself:default');
+		$this->flashMessage('Album bylo smazáno');
+		$this->redirect('Myself:default');
 	}
 
-	public function createComponentPlupload(){
+	public function createComponentPlupload() {
 		$plupload = $this->controlFactory->create();
 
 		$plupload->maxFileSize = '5mb';
 		$plupload->maxChunkSize = '1mb';
 		$plupload->allowedExtensions = 'jpg';
 
-		$slug = (string) $this->getParameter('slug');
+		$slug = (string)$this->getParameter('slug');
 		$id = $this->getIdFromSlug($slug);
 
-		$plupload->onFileUploaded[] = function(UploadQueue $uploadQueue) use($id) {
+		$plupload->onFileUploaded[] = function (UploadQueue $uploadQueue) use ($id) {
 			$upload = $uploadQueue->getLastUpload();
 
 			$name = $upload->getName();
-			$filename = self::photoDir.'/'.$id.'/'.$name;
-			$filepath = WWW_DIR.'/'. $filename;
+			$filename = self::photoDir . '/' . $id . '/' . $name;
+			$filepath = WWW_DIR . '/' . $filename;
 			$upload->move($filepath);
 
 			$values = [
@@ -201,19 +204,19 @@ class AlbumPresenter extends BasePresenter{
 			$this->gallery->addPhoto($values);
 
 			\LayoutHelpers::$thumbDirUri = 'albums/thumbs';
-			\LayoutHelpers::thumb($filename,150,100);
+			\LayoutHelpers::thumb($filename, 150, 100);
 		};
 
-		$plupload->onUploadComplete[] = function(UploadQueue $uploadQueue) use ($slug) {
+		$plupload->onUploadComplete[] = function (UploadQueue $uploadQueue) use ($slug) {
 			$this->flashMessage('Fotografie byli v pořádku přidány');
-			$this->redirect('view',$slug);
+			$this->redirect('view', $slug);
 		};
 
 		return $plupload;
 	}
 
-	public function pluploadSubmbited(FileUpload $file){
-		$slug = (string) $this->getParameter('slug');
+	public function pluploadSubmbited(FileUpload $file) {
+		$slug = (string)$this->getParameter('slug');
 		$id = $this->getIdFromSlug($slug);
 
 		$datum = new DateTime();
@@ -222,14 +225,14 @@ class AlbumPresenter extends BasePresenter{
 
 		if ($file->isImage()) {
 			if ($file->isOk()) {
-				$filename = self::photoDir.'/'.$id.'/'.$name;
-				$filepath = WWW_DIR.'/'. $filename;
+				$filename = self::photoDir . '/' . $id . '/' . $name;
+				$filepath = WWW_DIR . '/' . $filename;
 				$file->move($filepath);
 
 				$values = [
-				  'filename' => $name,
-				  'album_id' => $id,
-				  'date_add' => $datum
+					'filename' => $name,
+					'album_id' => $id,
+					'date_add' => $datum
 				];
 
 				$exif = exif_read_data($filepath);
@@ -241,60 +244,60 @@ class AlbumPresenter extends BasePresenter{
 				$this->gallery->addPhoto($values);
 
 				\LayoutHelpers::$thumbDirUri = 'albums/thumbs';
-				\LayoutHelpers::thumb($filename,150,100);
-			}else{
-				$this->flashMessage('Soubor '.$file->name.' nebyl v pořádnu nahrán');
-				$this->redirect('edit',$slug);
+				\LayoutHelpers::thumb($filename, 150, 100);
+			} else {
+				$this->flashMessage('Soubor ' . $file->name . ' nebyl v pořádnu nahrán');
+				$this->redirect('edit', $slug);
 			}
-		}else{
-			$this->flashMessage('Soubor '.$file->name.' není obrázek');
-			$this->redirect('edit',$slug);
+		} else {
+			$this->flashMessage('Soubor ' . $file->name . ' není obrázek');
+			$this->redirect('edit', $slug);
 		}
 	}
 
-	protected function createComponentSuperForm(){
+	protected function createComponentSuperForm() {
 		$form = new Form;
 
-		$form->addText('name','Název',30,50)
+		$form->addText('name', 'Název', 30, 50)
 			->setRequired('Vyplňte %label');
 
-		$form->addText('date', 'Datum', 10,10)
+		$form->addText('date', 'Datum', 10, 10)
 			->setRequired('Vyplňte datum začátku akce')
 			->setType('date')
 			->setDefaultValue(date_create()->format('Y-m-d'))
 			->addRule(Form::PATTERN, 'Datum musí být ve formátu RRRR-MM-DD', '[1-2]{1}\d{3}-[0-1]{1}\d{1}-[0-3]{1}\d{1}')
-			->setAttribute('class','date');
+			->setAttribute('class', 'date');
 
-		$form->addSelect('member_id','Uživatel',$this->members->getMembersArray(FALSE));
+		$form->addSelect('member_id', 'Uživatel', $this->members->getMembersArray(FALSE));
 
-		$form->addCheckBox('show_date','Upravit datum pořízení')
+		$form->addCheckBox('show_date', 'Upravit datum pořízení')
 			->setDefaultValue(FALSE)
 			->setAttribute('onclick', 'swapTitle(this)');
 
-		$form->addTextArea('text','Popis',30);
+		$form->addTextArea('text', 'Popis', 30);
 
 		$form->addMultiplier('photos', function (\Nette\Forms\Container $photo) {
 			$photo->addText('text', 'Popis', 30, 50);
 			$photo->addHidden('id');
 			$photo->addCheckBox('selected')
-				->setAttribute('class','select')
+				->setAttribute('class', 'select')
 				->setDefaultValue(FALSE);
 		}, 0);
 
 		$form->addSubmit('save', 'uložit změny')
-			->onClick[] =  [$this, 'SuperFormSave'];
+			->onClick[] = [$this, 'SuperFormSave'];
 
 		$form->addSubmit('delete', 'vymazat vybrané')
-			->onClick[] =  [$this, 'SuperFormDelete'];
+			->onClick[] = [$this, 'SuperFormDelete'];
 
 		$form->addSubmit('visible', 'změnit viditelnost')
-			->onClick[] =  [$this, 'SuperFormVisible'];
+			->onClick[] = [$this, 'SuperFormVisible'];
 
 		$form->addSubmit('turnLeft', 'otočit o 90° doleva')
-			->onClick[] =  [$this, 'SuperFormTurnLeft'];
+			->onClick[] = [$this, 'SuperFormTurnLeft'];
 
 		$form->addSubmit('turnRight', 'otočit o 90° doprava')
-			->onClick[] =  [$this, 'SuperFormTurnRight'];
+			->onClick[] = [$this, 'SuperFormTurnRight'];
 
 
 		$album = $this->getAlbumById($this->getParameter('slug'));
@@ -304,9 +307,9 @@ class AlbumPresenter extends BasePresenter{
 		return $form;
 	}
 
-	public function superFormSave(){
-		$slug = (string) $this->params['slug'];
-		$id = (int) $this->getIdFromSlug($slug);
+	public function superFormSave() {
+		$slug = (string)$this->params['slug'];
+		$id = (int)$this->getIdFromSlug($slug);
 
 		$values = $this['superForm']->getValues();
 		$values->date_update = new Datetime();
@@ -326,25 +329,24 @@ class AlbumPresenter extends BasePresenter{
 
 			if ($show_date) {
 				$datetime = $photo->text ? date_create($photo->text) : NULL;
-			if ($datetime == FALSE) $datetime = NULL;
+				if ($datetime == FALSE) $datetime = NULL;
 				$update['date_taken'] = $datetime;
-			}
-			else $update['text'] = self::nullString($photo->text);
+			} else $update['text'] = self::nullString($photo->text);
 
 			$this->gallery->getPhotoById($photo->id)->update($update);
 		}
 
 		$this->flashMessage('Album bylo upraveno');
-		$this->redirect('view',$id.'-'.$values->slug);
+		$this->redirect('view', $id . '-' . $values->slug);
 	}
 
-	private function getSuperFromSelected(){
+	private function getSuperFromSelected() {
 		$photos = $values = $this['superForm']->getValues()->photos;
 
 		if (!$photos) {
-			$slug = (string) $this->params['slug'];
-			$this->flashMessage('Musíte vybrat nějaká fotografie','error');
-			$this->redirect('edit',$slug);
+			$slug = (string)$this->params['slug'];
+			$this->flashMessage('Musíte vybrat nějaká fotografie', 'error');
+			$this->redirect('edit', $slug);
 		}
 
 		$selected = [];
@@ -353,41 +355,41 @@ class AlbumPresenter extends BasePresenter{
 		}
 
 		if (empty($selected)) {
-			$slug = (string) $this->params['slug'];
-			$this->flashMessage('Musíte vybrat nějaké fotografie','error');
-			$this->redirect('edit',$slug);
+			$slug = (string)$this->params['slug'];
+			$this->flashMessage('Musíte vybrat nějaké fotografie', 'error');
+			$this->redirect('edit', $slug);
 		}
 
 		return $selected;
 	}
 
-	public function superFormDelete(){
+	public function superFormDelete() {
 		$selected = $this->getSuperFromSelected();
 
 		$this->gallery->deletePhotos($selected);
 
-		$slug = (string) $this->params['slug'];
-		$this->flashMessage('Bylo smazáno '.count($selected).' fotografií');
-		$this->redirect('view',$slug);
+		$slug = (string)$this->params['slug'];
+		$this->flashMessage('Bylo smazáno ' . count($selected) . ' fotografií');
+		$this->redirect('view', $slug);
 	}
 
-	private function superFormImagesTurn($angle){
+	private function superFormImagesTurn($angle) {
 		$selected = $this->getSuperFromSelected();
 
-		$photos = $this->gallery->getPhotos()->where('id',$selected);
+		$photos = $this->gallery->getPhotos()->where('id', $selected);
 
 		foreach ($photos as $photo) {
-			$filename = self::photoDir.'/'.$photo->album_id.'/'.$photo->filename;
+			$filename = self::photoDir . '/' . $photo->album_id . '/' . $photo->filename;
 
 			$inputExifFile = new PelJpeg($filename);
 			$exif = $inputExifFile->getExif();
 			unset($inputExifFile);
 
-			$image =  Image::fromFile($filename);
-			$image->rotate($angle,0);
+			$image = Image::fromFile($filename);
+			$image->rotate($angle, 0);
 			$image->save($filename, 100, Image::JPEG);
 
-			if ($exif){
+			if ($exif) {
 				$outputExifFile = new PelJpeg($filename);
 				$outputExifFile->setExif($exif);
 				$outputExifFile->saveFile($filename);
@@ -397,29 +399,29 @@ class AlbumPresenter extends BasePresenter{
 		return $selected;
 	}
 
-	public function superFormTurnLeft(){
+	public function superFormTurnLeft() {
 		$selected = $this->superFormImagesTurn(90);
 
-		$slug = (string) $this->params['slug'];
-		$this->flashMessage('Doleva bylo otočeno '.count($selected).' fotografií');
-		$this->redirect('edit',$slug);
+		$slug = (string)$this->params['slug'];
+		$this->flashMessage('Doleva bylo otočeno ' . count($selected) . ' fotografií');
+		$this->redirect('edit', $slug);
 	}
 
-	 public function superFormTurnRight(){
+	public function superFormTurnRight() {
 		$selected = $this->superFormImagesTurn(-90);
 
-		$slug = (string) $this->params['slug'];
-		$this->flashMessage('Doprava bylo otočeno '.count($selected).' fotografií');
-		$this->redirect('edit',$slug);
+		$slug = (string)$this->params['slug'];
+		$this->flashMessage('Doprava bylo otočeno ' . count($selected) . ' fotografií');
+		$this->redirect('edit', $slug);
 	}
 
-	public function superFormVisible(){
+	public function superFormVisible() {
 		$selected = $this->getSuperFromSelected();
 
-		$this->gallery->getPhotos()->where('id',$selected)->update(['visible' => new SqlLiteral('NOT(`visible`)')]);
+		$this->gallery->getPhotos()->where('id', $selected)->update(['visible' => new SqlLiteral('NOT(`visible`)')]);
 
-		$slug = (string) $this->params['slug'];
-		$this->flashMessage('Bylo změněna viditelnost '.count($selected).' fotografií');
-		$this->redirect('view',$slug);
+		$slug = (string)$this->params['slug'];
+		$this->flashMessage('Bylo změněna viditelnost ' . count($selected) . ' fotografií');
+		$this->redirect('view', $slug);
 	}
 }
