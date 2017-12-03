8af1811e46b67fdb4a0ed704467ec517029878e7
irkallacz <mottl.jakub.janov@gmail.com>
2017-12-03 20:12:59 +0100

    Code reformat

--- app/MemberModule/presenters/DokumentyPresenter.php
+++ app/MemberModule/presenters/DokumentyPresenter.php
@@ -5,9 +5,10 @@ use App\Model\DokumentyService;
 use Google_Service_Drive;
 use Nette\Application\BadRequestException;
 use Nette\Application\Responses\TextResponse;
+use Nette\Http\Response;
 use Tracy\Debugger;
 
-class DokumentyPresenter extends LayerPresenter{
+class DokumentyPresenter extends LayerPresenter {
 
 	/** @var Google_Service_Drive @inject */
 	public $driveService;
@@ -15,30 +16,25 @@ class DokumentyPresenter extends LayerPresenter{
 	/** @var DokumentyService @inject */
 	public $dokumentyService;
 
-	/** @var \Nette\Http\Response @inject */
+	/** @var Response @inject */
 	public $httpResponse;
 
-	public function renderDefault(){
+	public function renderDefault() {
 		$this->template->TABLE_DOKUMENTY = DokumentyService::TABLE_DOKUMENTY;
 		$this->template->TABLE_DIRECTORIES = DokumentyService::TABLE_DIRECTORIES;
 
 		$this->template->dir = $this->dokumentyService->getDirectoryById(DokumentyService::DOCUMENT_DIR_ID);
 	}
 
-	public function actionGetPdf($id){
-        $file = $this->dokumentyService->getDokumentById($id);
+	public function actionGetPdf($id) {
+		$file = $this->dokumentyService->getDokumentById($id);
 
-        if (!$file){
-        	throw new BadRequestException('Soubor nenalezen');
-		}
+		if (!$file) throw new BadRequestException('Soubor nenalezen');
 
-		$response = $this->driveService->files->export($id,'application/pdf', ['alt' => 'media']);
+		$response = $this->driveService->files->export($id, 'application/pdf', ['alt' => 'media']);
 
-		$this->httpResponse->setHeader('Content-Disposition', 'attachment; filename="'.$file->name.'.pdf"');
+		$this->httpResponse->setHeader('Content-Disposition', 'attachment; filename="' . $file->name . '.pdf"');
 		$this->httpResponse->setHeader('Content-Type', 'application/pdf');
 		$this->sendResponse(new TextResponse($response->getBody()->getContents()));
-    }
-
-    public function renderTest(){
 	}
 }
