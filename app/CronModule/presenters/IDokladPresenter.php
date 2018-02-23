db5d6fdcb1c2cc61484218ae7740d3dc63f40f81
Vít Pekárek <pekarvit@fit.cvut.cz>
2018-02-23 02:59:39 +0100

    Check if authenticated, compare Contact Id which is stored in database

--- app/CronModule/presenters/IDokladPresenter.php
+++ app/CronModule/presenters/IDokladPresenter.php
@@ -9,6 +9,7 @@
 namespace App\CronModule\Presenters;
 
 use App\Model\UserService;
+use malcanek\iDoklad\auth\iDokladCredentials;
 use Nette\Database\Table\ActiveRow;
 use Nette\Database\Table\IRow;
 use malcanek\iDoklad;
@@ -30,78 +31,95 @@ class IdokladPresenter extends BasePresenter {
 	public $iDoklad;
 
 	/**
-	 * get all iDoklad contacts from VZS by IdentificationNumber and then
+	 * check if credetials are still valid, reauthenticate if not
+	 */
+	public function iDokladAuthenticate() {
+		$this->iDoklad->setCredentialsCallback(function ($credentials) {
+			file_put_contents('credentials.json', $credentials->toJson());
+		});
+		if (!file_exists('credentials.json') && empty($_GET['code'])) {
+			$this->iDoklad->authCCF();
+		}
+		$credentials = new iDokladCredentials(file_get_contents('credentials.json'), true);
+		$this->iDoklad->setCredentials($credentials);
+	}
+
+	/**
+	 * @param $id
+	 * get all iDoklad contacts, when find match with ID in database
 	 *  - update if there is a change
 	 *  - create new if not exists
 	 */
-	public function actionUpdate() {
+	public function actionUpdate(int $id = 200) {
 		$this->setView('default');
 		$items = [];
 		$users = $this->userService->getUsers(UserService::MEMBER_LEVEL);//->order('surname');
-		$this->iDoklad->authCCF();
+		$this->iDokladAuthenticate();
 		$request = new iDokladRequest('Contacts');
-		$filter = new iDokladFilter('IdentificationNumber', 'contains', 'VZSJBC');
-		$request->addFilter($filter)->setPageSize($users->count());
+		$request->setPageSize($id);
 		$response = $this->iDoklad->sendRequest($request);
 		$contacts = $response->getData();
-		foreach ($contacts as $contact) {
-			$id = substr($contact['IdentificationNumber'], 6);
-			$update_time = new Datetime($contact['DateLastChange']);
-			if ($users[$id]->date_update > $update_time) {
-				$request = new iDokladRequest('Contacts/' . $contact['Id']);
-				$request->addMethodType('PATCH');
-				$data = $this->setContactData($users[$id]);
-				$request->addPostParameters($data);
-				$response = $this->iDoklad->sendRequest($request);
-				if ($response->getCode() == 200) {
-					$items[$id] = "UPDATED";
-				} else {
-					$items[$id] = "FAILED";
+		foreach ($users as $user) {
+			foreach ($contacts as $contact) {
+				$contact_id = $contact['Id'];
+				if (!$contact_id) {
+					if ($this->contactCreate($user)) {
+						$items[$user->id] = $user->surname . " " . $user->name . " - CREATED";
+					} else {
+						$items[$user->id] = $user->surname . " " . $user->name . " - CREATING FAILED";
+					}
+					unset($users[$user->id]);
+					break;
+				}
+				if ($user->idoklad_id == $contact_id) {
+					$update_time = new Datetime($contact['DateLastChange']); //one hour shifted?
+					if ($user->date_update > $update_time) {
+						$request = new iDokladRequest('Contacts/' . $contact_id);
+						$request->addMethodType('PATCH');
+						$data = $this->setContactData($user);
+						$request->addPostParameters($data);
+						$response = $this->iDoklad->sendRequest($request);
+						if ($response->getCode() == 200) {
+							$items[$user->id] = $user->surname . " " . $user->name . " - UPDATED";
+						} else {
+							$items[$user->id] = $user->surname . " " . $user->name . " - FAILED";
+						}
+					} else {
+						$items[$user->id] = $user->surname . " " . $user->name . " - WITHOUT CHANGE";
+					}
+					unset($users[$user->id]);
+					break;
 				}
 			}
-			unset($users[$id]);
 		}
-		foreach ($users as $user) {
-			if ($this->contactCreate($user) == 200) {
-				$items[$user->id] = "CREATED";
-			} else {
-				$items[$user->id] = "CREATING FAILED";
-			}
+		if (!count($users)) {
+			echo('ERROR - some users left without action<br><br>');
 		}
 		$this->template->items = $items;
 	}
 
 	/**
-	 * go through the iDoklad contacts one by one by comparing "surname name" with CompanyName then
-	 *  - always update
-	 *  - does NOT create new if not exists
+	 * go through the iDoklad contacts one by one by comparing "surname name" (beware of duplicates) with CompanyName then
+	 *  - add idoklad_id to our database
 	 */
 	public function actionDefaultSync() {
 		$this->setView('default');
 		$items = [];
 		$users = $this->userService->getUsers(UserService::MEMBER_LEVEL);//->order('surname');
-		$this->iDoklad->authCCF();
+		$this->iDokladAuthenticate();
 		foreach ($users as $user) {
 			$request = new iDokladRequest('Contacts');
 			$filter = new iDokladFilter('CompanyName', '==', $user->surname . " " . $user->name);
 			$request->addFilter($filter);
 			$response = $this->iDoklad->sendRequest($request);
 			$person = $response->getData();
-			if (count($person) == 3) {
-				$items[$user->id] = "NOT FOUND";
+			if (count($person) != 1) {
+				$items[$user->id] = $user->surname . " " . $user->name . " - NOT FOUND";
 				continue;
 			}
-			$request = new iDokladRequest('Contacts/' . $person[0]['Id']);
-			$request->addMethodType('PATCH');
-			$data = $this->setContactData($user);
-			$request->addPostParameters($data);
-			$response = $this->iDoklad->sendRequest($request);
-			//echo $response->getCode();
-			if ($response->getCode() == 200) {
-				$items[$user->id] = "UPDATED";
-			} else {
-				$items[$user->id] = "FAILED";
-			}
+			$user->update(['idoklad_id' => $person[0]['Id']]);
+			$items[$user->id] = $user->surname . " " . $user->name . " - LOCALY UPDATED";
+
 		}
 		$this->template->items = $items;
 	}
@@ -117,25 +135,29 @@ class IdokladPresenter extends BasePresenter {
 			'City' => $user->mesto,
 			'Email' => $user->mail,
 			'Firstname' => $user->name,
-			'IdentificationNumber' => 'VZSJBC' . $user->id,
 			'Mobile' => $user->telefon,
 			'Street' => $user->ulice,
-			'Surname' => $user->name,
+			'Surname' => $user->name
 		);
 		return $data;
 	}
 
 	/**
 	 * @param IRow|ActiveRow $user
-	 * @return int
+	 * @return bool
 	 */
 	public function contactCreate($user) {
-		$this->iDoklad->authCCF();
+		$this->iDokladAuthenticate();
 		$request = new iDokladRequest('Contacts');
 		$request->addMethodType('POST');
 		$data = $this->setContactData($user);
 		$request->addPostParameters($data);
 		$response = $this->iDoklad->sendRequest($request);
-		return $response->getCode();
+		if ($response->getCode() == 200) {
+			$id = $response->getData()['Id'];
+			return $user->update(['idoklad_id' => $id]);
+		} else {
+			return false;
+		}
 	}
 }
