2b189b05ac75b2c92d7f88017208fc5f5a94f2cf
Vít Pekárek <pekarvit@fit.cvut.cz>
2018-02-24 13:24:09 +0100

    Credentional location change, iterate throught pages, repair timezone

--- app/CronModule/presenters/IDokladPresenter.php
+++ app/CronModule/presenters/IDokladPresenter.php
@@ -9,6 +9,7 @@
 namespace App\CronModule\Presenters;
 
 use App\Model\UserService;
+use DateTimeZone;
 use malcanek\iDoklad\auth\iDokladCredentials;
 use Nette\Database\Table\ActiveRow;
 use Nette\Database\Table\IRow;
@@ -24,6 +25,9 @@ use Tracy\Debugger;
  */
 class IdokladPresenter extends BasePresenter {
 
+	const PAGESIZE = 200;
+	const CREDENTIALS_FILENAME = 'credentials.json';
+
 	/** @var UserService @inject */
 	public $userService;
 
@@ -34,31 +38,37 @@ class IdokladPresenter extends BasePresenter {
 	 * check if credetials are still valid, reauthenticate if not
 	 */
 	public function iDokladAuthenticate() {
-		$this->iDoklad->setCredentialsCallback(function ($credentials) {
-			file_put_contents('credentials.json', $credentials->toJson());
+		$filePath = APP_DIR . '/../tmp/idoklad/' . self::CREDENTIALS_FILENAME;
+		$this->iDoklad->setCredentialsCallback(function ($credentials) use ($filePath) {
+			file_put_contents($filePath, $credentials->toJson());
 		});
-		if (!file_exists('credentials.json') && empty($_GET['code'])) {
+		if (!file_exists($filePath) || empty($_GET['code'])) {
 			$this->iDoklad->authCCF();
 		}
-		$credentials = new iDokladCredentials(file_get_contents('credentials.json'), true);
+		$credentials = new iDokladCredentials(file_get_contents($filePath), TRUE);
 		$this->iDoklad->setCredentials($credentials);
 	}
 
 	/**
-	 * @param $id
 	 * get all iDoklad contacts, when find match with ID in database
 	 *  - update if there is a change
 	 *  - create new if not exists
 	 */
-	public function actionUpdate(int $id = 200) {
+	public function actionUpdate() {
 		$this->setView('default');
 		$items = [];
 		$users = $this->userService->getUsers(UserService::MEMBER_LEVEL);//->order('surname');
 		$this->iDokladAuthenticate();
 		$request = new iDokladRequest('Contacts');
-		$request->setPageSize($id);
+		$request->setPageSize(self::PAGESIZE);
 		$response = $this->iDoklad->sendRequest($request);
 		$contacts = $response->getData();
+		$pages = $response->getTotalPages();
+		for ($i = 2; $i <= $pages; ++$i) {
+			$request->setPage($i);
+			$response = $this->iDoklad->sendRequest($request);
+			$contacts = array_merge($contacts, $response->getData());
+		}
 		foreach ($users as $user) {
 			foreach ($contacts as $contact) {
 				$contact_id = $contact['Id'];
@@ -72,7 +82,8 @@ class IdokladPresenter extends BasePresenter {
 					break;
 				}
 				if ($user->idoklad_id == $contact_id) {
-					$update_time = new Datetime($contact['DateLastChange']); //one hour shifted?
+					$update_time = new DateTime($contact['DateLastChange']); //one hour shifted?
+					$update_time->setTimezone(new DateTimeZone('+0100'));
 					if ($user->date_update > $update_time) {
 						$request = new iDokladRequest('Contacts/' . $contact_id);
 						$request->addMethodType('PATCH');
@@ -92,7 +103,7 @@ class IdokladPresenter extends BasePresenter {
 				}
 			}
 		}
-		if (!count($users)) {
+		if (count($users)) {
 			echo('ERROR - some users left without action<br><br>');
 		}
 		$this->template->items = $items;
@@ -157,7 +168,7 @@ class IdokladPresenter extends BasePresenter {
 			$id = $response->getData()['Id'];
 			return $user->update(['idoklad_id' => $id]);
 		} else {
-			return false;
+			return FALSE;
 		}
 	}
 }
