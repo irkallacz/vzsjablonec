61e8aa6ca78d6de037a9b68c54ec8d3fd8215b9e
irkallacz <mottl.jakub.janov@gmail.com>
2018-03-26 00:23:20 +0200

    Templates and files for sign domain

---
+++ www/subdom/sign/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// absolute filesystem path to this web root
+define('WWW_DIR', __DIR__);
+
+// absolute filesystem path to the application root
+define('APP_DIR', __DIR__ . '/../../../app');
+
+// uncomment this line if you must temporarily take down your site for maintenance
+//require __DIR__ . '/.maintenance.php';
+
+// load bootstrap file
+$container = require APP_DIR . '/bootstrap.php';
+
+$container->getByType(Nette\Application\Application::class)
+	->run(); 
