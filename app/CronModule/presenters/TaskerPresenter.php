66c1ae2a1b9f24853223c6915b391843b1d3545b
irkallacz <mottl.jakub.janov@gmail.com>
2018-04-25 23:56:50 +0200

    Cron Tasker for runnig tasks in selected hour

---
+++ app/CronModule/presenters/TaskerPresenter.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jakub
+ * Date: 25.4.2018
+ * Time: 23:18
+ */
+
+namespace App\CronModule\Presenters;
+
+
+class TaskerPresenter extends BasePresenter {
+
+	const TASKS = [
+		0 => 'Calendar:default',
+		1 => 'Drive:default',
+		2 => 'People:update',
+		3 => 'IDoklad:update',
+		4 => ':Photo:Thumbs:default',
+	];
+
+	public function actionDefault() {
+		$hour = date('G');
+
+		if (array_key_exists($hour, self::TASKS)) {
+			$task = self::TASKS[$hour];
+			$this->forward($task);
+		}
+	}
+}
