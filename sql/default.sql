662fd1bd485c38755ae1b873a7dea77b7508e2a6
irkallacz <mottl.jakub.janov@gmail.com>
2019-03-06 03:34:50 +0100

    Remove database dump commands, move database sql files into different folder, add default values into some tables

--- sql/default.sql
+++ sql/default.sql
@@ -1,6 +1,10 @@
 -- 2018-05-25T15:38:07+02:00 - mysql:host=localhost;dbname=vzsjablonec
 
 -- Table structure for table `akce`
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `akce`;
 CREATE TABLE `akce` (
@@ -8,45 +12,46 @@ CREATE TABLE `akce` (
   `name` varchar(60) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
   `perex` text CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
   `description` text CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
-  `message` text CHARACTER SET utf8 COLLATE utf8_czech_ci,
+  `message` text CHARACTER SET utf8 COLLATE utf8_czech_ci DEFAULT NULL,
   `date_start` datetime NOT NULL,
   `date_end` datetime NOT NULL,
   `date_deatline` datetime NOT NULL,
   `date_add` datetime NOT NULL,
   `date_update` datetime NOT NULL,
   `price` smallint(5) unsigned DEFAULT NULL,
-  `akce_for_id` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `akce_for_id` tinyint(3) unsigned NOT NULL DEFAULT 1,
   `place` varchar(50) NOT NULL,
-  `enable` tinyint(1) unsigned NOT NULL DEFAULT '1',
-  `confirm` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `visible` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `login_mem` tinyint(1) unsigned NOT NULL DEFAULT '1',
-  `login_org` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `enable` tinyint(1) unsigned NOT NULL DEFAULT 1,
+  `confirm` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `visible` tinyint(1) unsigned NOT NULL DEFAULT 1,
+  `login_mem` tinyint(1) unsigned NOT NULL DEFAULT 1,
+  `login_org` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `created_by` smallint(5) unsigned NOT NULL,
-  `modified_by` smallint(5) unsigned NULL,
+  `modified_by` smallint(5) unsigned DEFAULT NULL,
   `anketa_id` smallint(5) unsigned DEFAULT NULL,
   `forum_topic_id` smallint(5) unsigned DEFAULT NULL,
   `album_id` smallint(5) unsigned DEFAULT NULL,
   `calendarId` varchar(32) DEFAULT NULL,
-  `report` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `report` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `bill` varchar(60) DEFAULT NULL,
   `file` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `date_start` (`date_start`),
   KEY `enable` (`enable`),
   KEY `confirm` (`confirm`),
-  KEY `created_by` (`created_by`),
+  KEY `member_id` (`created_by`),
   KEY `akce_for_id` (`akce_for_id`),
   KEY `anketa_id` (`anketa_id`),
-  KEY `album_id` (`album_id`),
   KEY `forum_topic_id` (`forum_topic_id`),
-  CONSTRAINT `akce_ibfk_2` FOREIGN KEY (`akce_for_id`) REFERENCES `akce_for` (`id`),
-  CONSTRAINT `akce_ibfk_4` FOREIGN KEY (`anketa_id`) REFERENCES `anketa` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `akce_ibfk_5` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `akce_ibfk_6` FOREIGN KEY (`forum_topic_id`) REFERENCES `forum_post` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `akce_ibfk_7` FOREIGN KEY (`created_by`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `akce_ibfk_8` FOREIGN KEY (`modified_by`) REFERENCES `user` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=619 DEFAULT CHARSET=utf8;
+  KEY `album_id` (`album_id`),
+  KEY `modified_by` (`modified_by`),
+  CONSTRAINT `akce_ibfk_10` FOREIGN KEY (`anketa_id`) REFERENCES `anketa` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `akce_ibfk_11` FOREIGN KEY (`forum_topic_id`) REFERENCES `forum_post` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `akce_ibfk_12` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `akce_ibfk_13` FOREIGN KEY (`created_by`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `akce_ibfk_14` FOREIGN KEY (`modified_by`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `akce_ibfk_9` FOREIGN KEY (`akce_for_id`) REFERENCES `akce_for` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Table structure for table `akce_for`
 
@@ -57,13 +62,21 @@ CREATE TABLE `akce_for` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
+INSERT INTO `akce_for` (`id`, `text`) VALUES
+  (1,	'pro všechny členy'),
+  (2,	'pouze pro mládež'),
+  (3,	'pouze pro dospělé členy s platnou kvalifikací'),
+  (4,	'pro dopělé členy a členy mládeže s platnou kvalifikací'),
+  (5,	'pro dopělé členy'),
+  (6,	'pro veřejnost');
+
 -- Table structure for table `akce_member`
 
 DROP TABLE IF EXISTS `akce_member`;
 CREATE TABLE `akce_member` (
   `akce_id` smallint(6) NOT NULL,
   `user_id` smallint(5) unsigned NOT NULL,
-  `organizator` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `organizator` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `created_by` smallint(5) unsigned NOT NULL,
   `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`akce_id`,`user_id`,`organizator`),
@@ -82,8 +95,8 @@ CREATE TABLE `akce_rating_member` (
   `user_id` smallint(5) unsigned NOT NULL,
   `rating` tinyint(3) unsigned DEFAULT NULL,
   `message` text COLLATE utf8_czech_ci,
-  `anonymous` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `public` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `anonymous` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `public` tinyint(1) unsigned NOT NULL DEFAULT 1,
   `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`akce_id`,`user_id`),
   KEY `akce_id` (`akce_id`),
@@ -114,7 +127,7 @@ CREATE TABLE `album` (
   `slug` varchar(50) COLLATE utf8_czech_ci NOT NULL,
   `text` text COLLATE utf8_czech_ci,
   `private` text COLLATE utf8_czech_ci,
-  `visible` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `visible` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `user_id` smallint(5) unsigned NOT NULL,
   `date` date NOT NULL,
   `date_add` datetime NOT NULL,
@@ -124,7 +137,7 @@ CREATE TABLE `album` (
   UNIQUE KEY `slug` (`slug`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `album_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=188 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 -- Table structure for table `album_photo`
 
@@ -136,7 +149,7 @@ CREATE TABLE `album_photo` (
   `thumb` varchar(50) COLLATE utf8_czech_ci DEFAULT NULL,
   `text` varchar(50) COLLATE utf8_czech_ci DEFAULT NULL,
   `order` smallint(5) unsigned DEFAULT NULL,
-  `visible` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `visible` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `date_add` datetime NOT NULL,
   `date_taken` datetime DEFAULT NULL,
   `user_id` smallint(5) unsigned DEFAULT NULL,
@@ -145,7 +158,7 @@ CREATE TABLE `album_photo` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `album_photo_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
   CONSTRAINT `album_photo_ibfk_2` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=20486 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 -- Table structure for table `anketa`
 
@@ -161,7 +174,7 @@ CREATE TABLE `anketa` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `anketa_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 -- Table structure for table `anketa_member`
 
@@ -189,7 +202,7 @@ CREATE TABLE `anketa_odpoved` (
   PRIMARY KEY (`id`),
   KEY `anketa_id` (`anketa_id`),
   CONSTRAINT `anketa_odpoved_ibfk_1` FOREIGN KEY (`anketa_id`) REFERENCES `anketa` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 -- Table structure for table `dokumenty`
 
@@ -231,9 +244,16 @@ CREATE TABLE `forum` (
   `ord` smallint(5) unsigned NOT NULL,
   `title` varchar(50) COLLATE utf8_czech_ci NOT NULL,
   `text` text COLLATE utf8_czech_ci NOT NULL,
-  `hidden` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `hidden` tinyint(1) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+INSERT INTO `forum` (`id`, `ord`, `title`, `text`, `hidden`) VALUES
+  (1,	1,	'Obecná diskuze',	'Pokec o naší VZS, novinky, nápady, dotazy atd...',	0),
+  (2,	2,	'Akce',	'Debata k akcím, jak minulým tak budoucím',	0),
+  (3,	3,	'Bazar',	'Kupujeme, prodáváme, hledáme nebo sháníme',	0),
+  (4,	4,	'Webové stránky',	'Problémy se stránkami a návrhy na naše webovky. ',	0),
+  (5,	5,	'Různé',	'Vše mimo kategorie, diskuze netýkající se VZS a jiný brajgl.',	0);
 
 -- Table structure for table `forum_post`
 
@@ -248,8 +268,8 @@ CREATE TABLE `forum_post` (
   `date_update` datetime NOT NULL,
   `title` varchar(50) COLLATE utf8_czech_ci DEFAULT NULL,
   `text` text COLLATE utf8mb4_czech_ci NOT NULL,
-  `locked` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `hidden` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `locked` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `hidden` tinyint(1) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `forum_id` (`forum_id`),
   KEY `forum_topic_id` (`forum_topic_id`),
@@ -258,7 +278,7 @@ CREATE TABLE `forum_post` (
   CONSTRAINT `forum_post_ibfk_4` FOREIGN KEY (`forum_id`) REFERENCES `forum` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `forum_post_ibfk_5` FOREIGN KEY (`forum_topic_id`) REFERENCES `forum_post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `forum_post_ibfk_6` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4153 DEFAULT CHARSET=utf8mb4 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Table structure for table `hlasovani`
 
@@ -267,7 +287,7 @@ CREATE TABLE `hlasovani` (
   `id` tinyint(4) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(80) COLLATE utf8_czech_ci NOT NULL,
   `text` text COLLATE utf8_czech_ci NOT NULL,
-  `locked` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `locked` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `date_deatline` date NOT NULL,
   `date_add` datetime NOT NULL,
   `date_update` datetime NOT NULL,
@@ -275,7 +295,7 @@ CREATE TABLE `hlasovani` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `hlasovani_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 -- Table structure for table `hlasovani_member`
 
@@ -303,7 +323,7 @@ CREATE TABLE `hlasovani_odpoved` (
   PRIMARY KEY (`id`),
   KEY `hlasovani_id` (`hlasovani_id`),
   CONSTRAINT `hlasovani_odpoved_ibfk_2` FOREIGN KEY (`hlasovani_id`) REFERENCES `hlasovani` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Table structure for table `message`
 
@@ -322,7 +342,7 @@ CREATE TABLE `message` (
   KEY `message_type_id` (`message_type_id`),
   CONSTRAINT `message_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
   CONSTRAINT `message_ibfk_2` FOREIGN KEY (`message_type_id`) REFERENCES `message_type` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDBDEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 -- Table structure for table `message_type`
 
@@ -331,7 +351,7 @@ CREATE TABLE `message_type` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `type` varchar(50) COLLATE utf8_czech_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 -- Table structure for table `message_user`
 
@@ -372,7 +392,7 @@ CREATE TABLE `password_attempt` (
 DROP TABLE IF EXISTS `password_session`;
 CREATE TABLE `password_session` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `user_id` smallint(5) unsigned NOT NULL DEFAULT 0,
   `pubkey` varchar(32) DEFAULT NULL,
   `date_end` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
@@ -387,7 +407,9 @@ CREATE TABLE `rights` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_czech_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+INSERT INTO `rights` (`id`, `name`) VALUES (1,	'confirm');
 
 -- Table structure for table `roles`
 
@@ -396,7 +418,9 @@ CREATE TABLE `roles` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_czech_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+INSERT INTO `roles` (`id`, `name`) VALUES (0,	'user'), (1,	'member'), (2,	'board'), (3,	'admin');
 
 -- Table structure for table `user`
 
@@ -413,7 +437,7 @@ CREATE TABLE `user` (
   `ulice` varchar(30) COLLATE utf8_czech_ci DEFAULT NULL,
   `mail` varchar(50) COLLATE utf8_czech_ci NOT NULL,
   `mail2` varchar(50) COLLATE utf8_czech_ci DEFAULT NULL,
-  `send_to_second` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `send_to_second` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `telefon` char(9) COLLATE utf8_czech_ci DEFAULT NULL,
   `telefon2` char(9) COLLATE utf8_czech_ci DEFAULT NULL,
   `text` text COLLATE utf8_czech_ci,
@@ -427,7 +451,7 @@ CREATE TABLE `user` (
   UNIQUE KEY `mail_mail2` (`mail`, `mail2`),
   KEY `role` (`role`),
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role`) REFERENCES `roles` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=150 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 -- Table structure for table `user_log`
 
---
+++ sql/wordpress.sql
@@ -0,0 +1,62 @@
+-- 2017-12-03T21:21:36+01:00 - mysql:host=localhost;dbname=wordpress
+
+-- Table structure for table `nnx2_posts`
+
+DROP TABLE IF EXISTS `nnx2_posts`;
+CREATE TABLE `nnx2_posts` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `post_author` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `post_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `post_date_gmt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `post_content` longtext COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `post_title` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `post_excerpt` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `post_status` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'publish',
+  `comment_status` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'open',
+  `ping_status` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'open',
+  `post_password` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `post_name` varchar(200) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `to_ping` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `pinged` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `post_modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `post_modified_gmt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `post_content_filtered` longtext COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `post_parent` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `guid` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `menu_order` int(11) NOT NULL DEFAULT '0',
+  `post_type` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'post',
+  `post_mime_type` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `comment_count` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `post_name` (`post_name`(191)),
+  KEY `type_status_date` (`post_type`,`post_status`,`post_date`,`ID`),
+  KEY `post_parent` (`post_parent`),
+  KEY `post_author` (`post_author`)
+) ENGINE=InnoDB AUTO_INCREMENT=1954 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+-- Table structure for table `nnx2_term_relationships`
+
+DROP TABLE IF EXISTS `nnx2_term_relationships`;
+CREATE TABLE `nnx2_term_relationships` (
+  `object_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `term_taxonomy_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `term_order` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`object_id`,`term_taxonomy_id`),
+  KEY `term_taxonomy_id` (`term_taxonomy_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+-- Table structure for table `nnx2_term_taxonomy`
+
+DROP TABLE IF EXISTS `nnx2_term_taxonomy`;
+CREATE TABLE `nnx2_term_taxonomy` (
+  `term_taxonomy_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `term_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `taxonomy` varchar(32) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `description` longtext COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `parent` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `count` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`term_taxonomy_id`),
+  UNIQUE KEY `term_id_taxonomy` (`term_id`,`taxonomy`),
+  KEY `taxonomy` (`taxonomy`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
