029f669b66d54036bce120b486d407ead3dfb64b
irkallacz <mottl.jakub.janov@gmail.com>
2019-08-04 16:05:03 +0200

    Move javascript into separate file

---
+++ www/vzsjablonec/js/member.billing.js
@@ -0,0 +1,33 @@
+function recount(name, oldPrice, newPrice) {
+	var el = document.getElementById('billing-'+name);
+	const value = parseFloat(el.value);
+	el.value = value + newPrice - oldPrice;
+
+	const incomes = parseFloat(document.getElementById('billing-income').value);
+	const expenses = parseFloat(document.getElementById('billing-expense').value);
+	document.getElementById('billing-final').value = incomes - expenses;
+}
+
+document.querySelectorAll('input.remove').forEach(function (el) {
+	el.addEventListener('click', function () {
+		const row       = el.parentElement.parentElement;
+		const category  = row.parentElement.parentElement.id;
+		const old       = parseFloat(row.querySelector('input.final').value);
+
+		recount(category, old, 0);
+		row.remove();
+	});
+});
+
+document.querySelectorAll('input.price, input.count').forEach(function (el) {
+	el.addEventListener('change', function () {
+		const row       = el.parentElement.parentElement;
+		const category  = row.parentElement.parentElement.id;
+		const final     = parseFloat(row.querySelector('input.price').value) * parseInt(row.querySelector('input.count').value);
+		var   field     = row.querySelector('input.final');
+		const old       = parseFloat(field.value);
+		field.value = final;
+
+		recount(category, old, final);
+	})
+});
