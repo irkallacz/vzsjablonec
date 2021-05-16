5de13de5b18ccc4235f83cbecb000f590d962461
Vít Pekárek <vit.pekarek.jr@gmail.com>
2021-05-16 02:21:09 +0200

    Videos from YT channel in news, info stored in DB

---
+++ app/CronModule/presenters/YoutubePresenter.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\CronModule\Presenters;
+
+use App\Model\YoutubeService;
+use DateTimeZone;
+
+class YoutubePresenter extends BasePresenter {
+
+	/** @var YoutubeService @inject */
+	public $youtubeService;
+
+	public function actionDefault() {
+		$youtube = new \Google_Service_YouTube($this->youtubeService->googleClient);
+
+		$videos = [];
+		$playlists = $youtube->channels->listChannels('contentDetails', ['id' => YoutubeService::CHANNEL_ID]);
+
+		foreach ($playlists as $playlist) {
+			$playlistId = $playlist->contentDetails->relatedPlaylists->uploads;
+			$nextPageToken = '';
+
+			while (!is_null($nextPageToken)) {
+				$playlistItems = $youtube->playlistItems->listPlaylistItems('snippet', [
+					'playlistId' => $playlistId,
+					'maxResults' => 50,
+					'pageToken'  => $nextPageToken
+				]);
+				foreach ($playlistItems as $playlistItem) {
+					$videos[] = $playlistItem->snippet;
+				}
+				$nextPageToken = $playlistItems->nextPageToken;
+			}
+		}
+
+		$this->template->videos = $videos;
+
+		foreach ($videos as $video) {
+			$publishedAt = new \DateTime($video->publishedAt);
+			$publishedAt->setTimezone(new DateTimeZone('Europe/Prague'));
+			$this->youtubeService->addFile([
+					'videoId'     => $video->resourceId->videoId,
+					'title'       => $video->title,
+					'publishedAt' => $publishedAt
+				]);
+		}
+	}
+}
---
+++ app/models/YoutubeService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Model\MessageService;
+
+namespace App\Model;
+
+use Nette\Database\Context;
+use Nette\Database\ResultSet;
+use Nette\Database\Table\Selection;
+use Nette\Utils\DateTime;
+
+class YoutubeService extends DatabaseService {
+
+	const TABLE_VIDEA = 'videa';
+	const CHANNEL_ID = 'UCR9cGiK9bpjsOBB3OjBOkDg';
+
+	/** @var \Google_Client */
+	public $googleClient;
+
+	/**
+	 * YoutubeService constructor.
+	 * @param \Google_Client $googleClient
+	 * @param Context $database
+	 */
+	public function __construct(\Google_Client $googleClient, Context $database) {
+		parent::__construct($database);
+		$this->googleClient = $googleClient;
+	}
+
+	/**
+	 * @return Selection
+	 */
+	public function getVideo() {
+		return $this->database->table(self::TABLE_VIDEA);
+	}
+
+	/**
+	 * @param DateTime $date
+	 * @return Selection
+	 */
+	public function getVideoNews(DateTime $date) {
+		return $this->getVideo()
+			->where('publishedAt > ?', $date)
+			->order('publishedAt DESC');
+	}
+
+	/**
+	 * @param array $values
+	 * @return ResultSet
+	 */
+	public function addFile(array $values) {
+		return $this->database->query("INSERT INTO " . self::TABLE_VIDEA, $values, "ON DUPLICATE KEY UPDATE publishedAt = ", $values['publishedAt']);
+	}
+}
