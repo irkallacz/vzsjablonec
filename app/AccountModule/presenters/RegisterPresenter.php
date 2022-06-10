4be088a752f66c35e099d8f7b6f8db8b142c4700
irkallacz <mottl.jakub.janov@gmail.com>
2022-06-10 11:48:24 +0200

    New registration mail - rename user -> member due to collision with read only property

--- app/AccountModule/presenters/RegisterPresenter.php
+++ app/AccountModule/presenters/RegisterPresenter.php
@@ -96,7 +96,7 @@ class RegisterPresenter extends BasePresenter {
 	private function addRegistrationMail(IRow $user){
 		$template = $this->createTemplate();
 		$template->setFile(__DIR__ . '/../../presenters/templates/Mail/newRegistration.latte');
-		$template->user = $user;
+		$template->member = $user;
 
 		$message = new MessageService\Message(MessageService\Message::REGISTRATION_NEW_TYPE);
 		$message->setSubject('Nová registrace uživatele');
