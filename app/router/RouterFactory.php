d77f69538383f755a46872ada641343556c31903
jakub <mottl.jakub.janov@gmail.com>
2024-02-17 14:11:21 +0100

    Router service - chance member to intranet

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -16,13 +16,13 @@ class RouterFactory {
 
 		$member = new RouteList('Member');
 
-		$member[] = new Route('//member.%domain%/forum/view-post/<id>','Forum:post', Route::ONE_WAY);
-		$member[] = new Route('//member.%domain%/forum/view/<id>','Forum:topic', Route::ONE_WAY);
+		$member[] = new Route('//intranet.%domain%/forum/view-post/<id>','Forum:post', Route::ONE_WAY);
+		$member[] = new Route('//intranet.%domain%/forum/view/<id>','Forum:topic', Route::ONE_WAY);
 
-		$member[] = new Route('//member.%domain%/akce/[year/<yp-year>]','Akce:default');
-		$member[] = new Route('//member.%domain%/attendance/[year/<yp-year>]','Attendance:default');
-		$member[] = new Route('//member.%domain%/forum/<action>/<id>[/page/<vp-page>]','Forum:view');
-		$member[] = new Route('//member.%domain%/<presenter>/<action>[/<id>]', 'News:default');
+		$member[] = new Route('//intranet.%domain%/akce/[year/<yp-year>]','Akce:default');
+		$member[] = new Route('//intranet.%domain%/attendance/[year/<yp-year>]','Attendance:default');
+		$member[] = new Route('//intranet.%domain%/forum/<action>/<id>[/page/<vp-page>]','Forum:view');
+		$member[] = new Route('//intranet.%domain%/<presenter>/<action>[/<id>]', 'News:default');
 
 		$account = new RouteList('Account');
 		$account[] = new Route('//account.%domain%/<presenter>/<action>[/<id>]', 'Sign:default');
