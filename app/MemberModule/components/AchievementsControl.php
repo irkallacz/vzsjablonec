4102a2a516cb2a5667f9a5af372caa80bbed7741
irkallacz <mottl.jakub.janov@gmail.com>
2025-02-09 09:49:56 +0100

    Achievement control - refactor

--- app/MemberModule/components/AchievementsControl.php
+++ app/MemberModule/components/AchievementsControl.php
@@ -20,29 +20,23 @@ final class AchievementsControl extends LayerControl
 	protected $userService;
 
 	/**
-	 * @var int
-	 */
-	protected $memberId;
-
-	/**
 	 * AchievementsControl constructor.
 	 * @param AchievementsService $achievementsService
 	 * @param UserService $userService
 	 * @param $userId
 	 */
-	public function __construct(AchievementsService $achievementsService, UserService $userService, int $memberId)
+	public function __construct(AchievementsService $achievementsService, UserService $userService)
 	{
 		$this->achievementsService = $achievementsService;
 		$this->userService = $userService;
-		$this->memberId = $memberId;
 	}
 
-	public function render()
+	public function render(int $memberId)
 	{
 		$this->template->setFile(__DIR__ . '/AchievementsControl.latte');
 
-		$this->template->badgesCount = $this->achievementsService->getBadgesForUser($this->memberId)->count('achievement_id');
-		$this->template->badges = $this->achievementsService->getBadgesAchievements($this->memberId);
+		$this->template->badgesCount = $this->achievementsService->getBadgesForUser($memberId)->count('achievement_id');
+		$this->template->badges = $this->achievementsService->getBadgesAchievements($memberId);
 
 		$users = $this->userService->getUsers(UserService::MEMBER_LEVEL)->count('id');
 		$this->template->goldies = $this->achievementsService->getBadgesCount()->having('COUNT(`id`) < ?', $users / 10)->fetchPairs(null, 'achievement_id');
